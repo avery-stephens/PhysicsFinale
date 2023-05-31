1b1167f390e241c93c7253158de31d7783aaf921
avery-stephens <astephens@student.neumont.edu>
2023-05-31 11:12:00 -0600

    Added Parallax
    folder pooped itself

--- Assets/Scripts/Dragable.cs
+++ Assets/Scripts/Dragable.cs
@@ -5,21 +5,55 @@ using UnityEngine.EventSystems;
 
 public class Dragable : MonoBehaviour
 {
-    
-    Vector3 mousePositionOffset;
+	Vector3 mousePositionOffset;
+	private Vector3 GetMouseWorldPosition()
+	{
+		//capture mouse position & return WorldPoint
+		return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
 
-    private Vector3 GetMouseWorldPosition()
-    {
-        //capture mouse position & return WorldPoint
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
+	private void OnMouseDown()
+	{
+		mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+		if (TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+		{
+			rb.gravityScale = 0;
+		}
+	}
+	private void OnMouseDrag()
+	{
+		if (Input.GetKey(KeyCode.Q))
+		{
+			transform.Rotate(Vector3.forward * 180 * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.E))
+		{
+			transform.Rotate(Vector3.forward * -180 * Time.deltaTime);
+		}
+		transform.position = GetMouseWorldPosition() + mousePositionOffset;
+	}
 
-    private void OnMouseDown()
-    {
-        mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
-    }
-    private void OnMouseDrag()
-    {
-        transform.position = GetMouseWorldPosition() + mousePositionOffset;
-    }
+	private void OnMouseEnter()
+	{
+		if (TryGetComponent<SpriteRenderer>(out SpriteRenderer sr))
+		{
+			sr.color = Color.cyan;
+		}
+	}
+
+	private void OnMouseExit()
+	{
+		if (TryGetComponent<SpriteRenderer>(out SpriteRenderer sr))
+		{
+			sr.color = Color.white;
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		if (TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+		{
+			rb.gravityScale = 1;
+		}
+	}
 }
