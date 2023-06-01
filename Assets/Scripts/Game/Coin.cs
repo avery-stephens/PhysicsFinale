dc2123e538209b42211abf14481ff25a44920ca7
avery-stephens <astephens@student.neumont.edu>
2023-06-01 17:23:42 -0600

    Added Audio
    might need fixing

--- Assets/Scripts/Game/Coin.cs
+++ Assets/Scripts/Game/Coin.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(AudioSource))]
 public class Coin : MonoBehaviour
 {
+    [SerializeField] public AudioSource m_Clip;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -12,6 +14,7 @@ public class Coin : MonoBehaviour
             GameManager.Instance.AddCoin();
             GameManager.Instance.AddScore(100);
             gameObject.SetActive(false);
+            m_Clip.Play();
         }
     }
 }
