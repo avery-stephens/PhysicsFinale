437d47aafab3d7ee168541d9a2c3b17db79f9054
LesserFlame <94986192+LesserFlame@users.noreply.github.com>
2023-06-01 18:13:09 -0600

    Audio
    audio

--- Assets/Scripts/Game/Coin.cs
+++ Assets/Scripts/Game/Coin.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(AudioSource))]
+//[RequireComponent(typeof(AudioSource))]
 public class Coin : MonoBehaviour
 {
-    [SerializeField] public AudioSource m_Clip;
+    //[SerializeField] public AudioSource m_Clip;
+    [SerializeField] private GameObject m_CoinEffect;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameManager.Instance.AddCoin();
             GameManager.Instance.AddScore(100);
+            Instantiate(m_CoinEffect);
             gameObject.SetActive(false);
-            m_Clip.Play();
         }
     }
 }
